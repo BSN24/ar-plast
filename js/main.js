056d1077c4ec2352b53a838a35e5c8c72663fd57
bsn24 <prizrak24@gmail.com>
2019-08-16 18:46:16 +0600

    Добавил полифил для спрайтов svg в IE js/lib/svgxuse

--- js/main.js
+++ js/main.js
@@ -34,4 +34,41 @@ $(document).ready(function () {
 
     });
 
+    //mobile menu
+    (function () {
+        $(".hamburger-btn").click(function () {
+            $(".mobile-nav").addClass("mobile-nav--active");
+        });
+
+        $(".mobile-nav__btn").click(function () {
+            $(".mobile-nav").removeClass("mobile-nav--active");
+        });
+
+    })();
+
+
+    //Filter
+    (function () {
+
+        $(".filter-btn-open").click(function () {
+            $(".filter").addClass("filter--open");
+        });
+
+        $(".filter__close").click(function () {
+            $(".catalog__filter").removeClass("filter--open");
+        });
+
+        $(document).click(function (e){
+            var filter = $(".catalog__filter");
+            var filterBtn = $(".filter-btn-open");
+
+            if (window.matchMedia("(max-width: 767.98px)").matches) {
+                if ((!filter.is(e.target) && !filterBtn.is(e.target)) && filter.has(e.target).length === 0) {
+                    filter.removeClass("filter--open");
+                }
+            }
+        });
+
+    })();
+
 });
